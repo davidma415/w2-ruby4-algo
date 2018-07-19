9ce7bb36a178f842d070aecfcf013a7ea9fd56c0
David Ma <davidma415@gmail.com>
2018-07-19 00:34:55 -0400

    Removed instructions from final copy

--- exercise01.rb
+++ exercise01.rb
@@ -1,11 +1,3 @@
-# Save the direction of train 111 into a variable.
-# Save the frequency of train 80B into a variable.
-# Save the direction of train 610 into a variable.
-# Create an empty array. Iterate through each train and add the name of the train into the array if it travels north.
-# Do the same thing for trains that travel east.
-# You probably just ended up rewriting some of the same code. Move this repeated code into a method that accepts a direction and a list of trains as arguments, and returns a list of just the trains that go in that direction. Call this method once for north and once for east in order to DRY up your code.
-# Pick one train and add another key/value pair for the first_departure_time. For simplicity, assume the first train always leave on the hour. You can represent this hour as an integer: 6 for 6:00am, 12 for noon, 13 for 1:00pm, etc.
-
 trains = [
 {train: "72C", frequency_in_minutes: 15, direction: "north"},
 {train: "72D", frequency_in_minutes: 15, direction: "south"},
